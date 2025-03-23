8bc35c5f4a6dc7c1d9888c3fd3f0414eb6f8dcb5
sara53 <sara.atef001@gmail.com>
2025-03-23 10:23:40 +0200

    day04

---
+++ Day04/demo.js
@@ -0,0 +1,7 @@
+var img = document.images[0];
+
+// var link1 = document.links[0];
+// var link2 = document.links[1];
+// var link3 = document.links[2];
+
+function execute() {}
---
+++ Day04/main.js
@@ -0,0 +1,84 @@
+// var parent = document.getElementById("parent");
+
+// var elements = parent.getElementsByClassName("info");
+
+// for (var i = 0; i < elements.length; i++) {
+// 	console.log(elements[i]);
+// }
+// console.log(elements[0]);
+// console.log(elements[1]);
+// object -- properties - methods
+//console.log(document);
+
+/**------------------------------- */
+// var element = document.querySelector("button"); // first
+
+// console.log(element);
+
+// for (var i = 0; i < elements.length; i++) {
+// 	console.log(elements[i]);
+// }
+// var parent = document.getElementById("parent");
+
+// console.log(parent.children);
+// console.log(parent.firstChild); //
+// console.log(parent.childNodes);
+// console.log(parent.firstChild);
+// console.log(parent.firstElementChild);
+/**---------------- change Text ----------- */
+// var p = document.getElementsByClassName("info")[0];
+
+// function changeText() {
+// 	//p.innerText = "<h1>Hello dotnet track</h1>";
+// 	// console.log(p.innerText); // get
+// 	// p.innerText = "<h1>Hello dotnet track</h1>"; // override
+// }
+/**---------------- change input value ----------- */
+
+// var input = document.getElementsByTagName("input")[0];
+// var span = document.getElementById("output");
+
+// var inputs = document.getElementsByName("choose");
+// function execute() {
+// 	for (var i = 0; i < inputs.length; i++) {
+// 		if (inputs[i].checked) {
+// 			console.log(inputs[i].value);
+// 		}
+// 	}
+// 	// span.innerHTML = input.value;
+// 	// input.value = "dotnet";
+// 	// get
+// 	// console.log(input.value);
+// }
+/**---------------- Change Style ----------- */
+// var p = document.getElementById("info");
+// function changeStyle() {
+// 	// p.classList.toggle("jsClass");
+// 	// p.classList.remove("testData");
+// 	// p.classList.add("jsClass");
+// 	// console.log(p.classList);
+// }
+// function changeStyle() {
+// 	p.className += " jsClass";
+// }
+// function changeStyle() {
+// 	p.style.backgroundColor = "#1390b6";
+// 	p.style.padding = "20px";
+// 	p.style.borderRadius = "10px";
+// 	p.style.display = "none";
+// }
+/** Deal With Attributes  */
+//img.setAttribute("src", "2.jpg");
+// console.log(img.getAttribute("src"));
+// if (link1.hasAttribute("class")) {
+// 	link1.removeAttribute("class");
+// } else {
+// 	link1.setAttribute("class", "link-item");
+// }
+// link2.setAttribute("class", "link-item");
+// override
+//link2.setAttribute("href", "www.google.com");
+// link1.setAttribute("class", "test"); // change Style
+// console.log(link1.getAttribute("id"));
+// console.log(link2.getAttribute("href"));
+// console.log(link1.getAttribute("class"));
